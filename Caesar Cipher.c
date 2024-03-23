1934bae09a7bfb4b873d7baf6861191d9caf5d43
Beyza Nur Koç <134156951+beyzanurkoc@users.noreply.github.com>
2024-03-24 00:24:57 +0300

    Update Caesar Cipher.c

--- Caesar Cipher.c
+++ Caesar Cipher.c
@@ -1,4 +1,6 @@
-/*In this question, you are tasked with taking two characters and an integer represents the key for Caesar Cipher and shows Encrypted characters with a given key value. Caesar cipher is a basic encryption method where each letter in the plaintext is shifted a certain number of positions up or down the alphabet. This numerical shift, known as the "key," is typically chosen by the user. For instance, with a key of 3:
+/*In this question, you are tasked with taking two characters and an integer represents the key for Caesar Cipher and shows Encrypted characters with a given key value.
+Caesar cipher is a basic encryption method where each letter in the plaintext is shifted a certain number of positions up or down the alphabet.
+This numerical shift, known as the "key," is typically chosen by the user. For instance, with a key of 3:
 - 'A' becomes 'D'
 - 'B' becomes 'E'
 - 'C' becomes 'F'
@@ -7,6 +9,7 @@
 - 'Y' becomes 'B'
 - 'Z' becomes 'C'
 While historically significant, the Caesar cipher is a straightforward and easily breakable encryption technique when confronted with modern cryptographic methods.*/
+
 #include <stdio.h>
 
 int main()
